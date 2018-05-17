1e9c169eae227d7116df2723db67a20f8ef431c7
15804 <15804@hs-90541-103.lan.summit.k12.nj.us>
2018-05-17 09:24:19 -0400

    Initial commit

---
+++ Button.java
@@ -0,0 +1,56 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Button here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Button extends Actor
+{
+    private int x, y, w, h;
+    private String text;
+    
+    public Button(int x, int y, int w, int h, String text) {
+        this.x = x;
+        this.y = y;
+        
+        this.w = w;
+        this.h = h;
+        this.text = text;
+        
+        this.getImage().scale(w, h);
+        this.setLocation(x, y);
+    }
+    
+    /**
+     * Act - do whatever the Button wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        this.getWorld().showText(this.text, this.x, this.y);
+        this.handleClicked();
+    }    
+    
+    public boolean isClicked() {
+        return Greenfoot.mouseClicked(this);
+    }
+    
+    public void handleClicked() {
+        if(this.isClicked()) {
+            this.onClick();
+        }
+    }
+    
+    public void onClick() {
+        System.out.println("HELLO WORLD");
+    }
+    
+    public int getx(){
+        return this.x;
+    }
+    public int gety(){
+        return this.y;
+    }
+}
---
+++ MyWorld.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        Button x = new Button(100, 100, 300, 80, "button");
+        addObject(x, x.getx(), x.gety());
+    }
+}
