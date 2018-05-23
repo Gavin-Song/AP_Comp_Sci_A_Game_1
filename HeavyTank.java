04cd78ee247e02da5b02ca3233995e037eb2e7b6
Gavin Song <gavin.song.bc@gmail.com>
2018-05-23 17:24:31 -0400

    Add even more units

---
+++ HeavyTank.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HeavyTank here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HeavyTank extends WombatLandUnit
+{
+    public HeavyTank(){
+        super(80, 40, 600, "WA-Armadillo", "Heavy Landcruiser"); // width, height, health, name, desc
+        this.setFriction(false);
+    }
+    
+    public void createTurrets() {
+        this.getTurrets().add(new WeakTankTurret(10, 5, -10, 5, "wombat"));
+    }
+    
+    public void act() 
+    {
+        this.setvx(-2);
+        super.act();
+    }    
+}
